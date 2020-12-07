a98e0dd6d861bc11b50bc0ab9c1ea8c6e1ab06a6
Xearta <rsmcwow@gmail.com>
2020-12-07 09:08:11 -0500

    Changed the styles to a stylesheet

--- App.js
+++ App.js
@@ -3,12 +3,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View>
-        <TextInput
-          placeholder='Enter Goal'
-          style={{ borderColor: 'Black', borderWidth: 1, padding: 10 }}
-        />
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter Goal' style={styles.input} />
         <Button title='ADD' />
       </View>
       <View></View>
@@ -18,9 +15,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
   },
 });
